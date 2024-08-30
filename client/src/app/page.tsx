99eee18166e562a812349d0730d24b8a26b1dbe0
“Shashank-21” <shashankbr21494@gmail.com>
2024-08-30 09:16:55 +0530

    Home page changes

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -11,20 +11,22 @@ export default function Home() {
         height={369.26}
         className="w-3/4 md:w-auto"
       />
-      <div className="flex flex-col items-center justify-center space-y-4 md:space-y-6">
-        <p className="text-3xl md:text-5xl text-center font-thin leading-relaxed">
-          Focus on{" "}
-          <span className="border-b-4 md:border-b-8 pb-1 border-lime-green font-medium">
-            your best
-          </span>
-        </p>
-        <p className="text-3xl md:text-5xl text-center font-thin leading-relaxed">
-          Let{" "}
-          <span className="border-b-4 md:border-b-8 pb-1 border-lime-green font-medium">
-            technology
-          </span>{" "}
-          take care of the rest!
-        </p>
+      <div className="flex flex-col items-center justify-center space-y-8 md:space-y-12">
+        <div className="flex flex-col items-center justify-center space-y-4 md:space-y-6">
+          <p className="text-3xl md:text-5xl text-center font-thin leading-relaxed">
+            Focus on{" "}
+            <span className="border-b-4 md:border-b-8 pb-1 border-lime-green font-medium">
+              your best
+            </span>
+          </p>
+          <p className="text-3xl md:text-5xl text-center font-thin leading-relaxed">
+            Let{" "}
+            <span className="border-b-4 md:border-b-8 pb-1 border-lime-green font-medium">
+              technology
+            </span>{" "}
+            take care of the rest!
+          </p>
+        </div>
 
         <p className="text-lg md:text-xl text-center font-light leading-relaxed md:leading-loose max-w-3xl mt-6">
           Simplify your workflow and reclaim your time with smart automation
